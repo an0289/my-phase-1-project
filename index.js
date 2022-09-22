4bf1b16be6fd3f3a144b9e5d851fd53398266452
Alexandra <alexandraneff@live.com>
2022-09-22 18:15:15 -0500

    Fetch for drink name

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ function grabById(id) {
     return document.getElementById(id)
 }
 
-// document.getElementById("toggle").onclick = function() {
-//    if (grabById("container").classList.contains("closed")) {
-//     grabById("container").classList.remove("closed")
-//    } else {
-//     grabById("container").classList.add("closed")
-//    }
-// } 
-
 document.getElementById("toggle").addEventListener("click", function() {
     if (grabById("container").classList.contains("closed")) {
      grabById("container").classList.remove("closed")
@@ -18,4 +10,11 @@ document.getElementById("toggle").addEventListener("click", function() {
     }
  } )
 
-
+//Fetch function to find drinks by their name
+function getDrinksByName(drinkName) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        })
+    }
