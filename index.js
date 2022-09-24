d375bfe3fa4f5d09f242e1b802bd5be6ac916fb0
Alexandra <alexandraneff@live.com>
2022-09-24 13:38:00 -0500

    Created drinksArray for data.drinks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function grabById(id) {
-    return document.getElementById(id)
-}
+// function grabById(id) {
+//     return document.getElementById(id)
+// }
 
  
 //Fetch function to find drinks by their name
@@ -8,20 +8,12 @@ function getDrinksByName(drinkName) {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
     .then(resp => resp.json())
     .then(data => {
-        data.drinks.forEach(drinkName => {
+        drinksArray = data.drinks
+        drinksArray.forEach(drinkName => {
             renderRecipeCard(drinkName)
         })
-        console.log(data.drinks)
+        console.log(drinksArray)
         })
-
-    // Event Listener to display ingredients/instructions
-//    document.getElementById("toggle").addEventListener("click", function() {
-//     if (grabById("container").classList.contains("closed")) {
-//      grabById("container").classList.remove("closed")
-//     } else {
-//      grabById("container").classList.add("closed")
-//     }
-// })
 }
 
 //Event Listener Submit 
@@ -32,6 +24,13 @@ submitForm.addEventListener('submit', (e) => {
     getDrinksByName(search.value)
     submitForm.reset()
 })
+
+// submitFrom.addEventListener('change', (e) => {
+//     if (e.search.value === drinkName) {
+//         return 
+//     }
+//    }) 
+
 
 //Function to dynamical render the card 
 function renderRecipeCard(drinkInfo) {
@@ -266,29 +265,3 @@ function renderRecipeCard(drinkInfo) {
       
 }
 
-    //Function to place the grabbed data into the card 
-//     function renderRecipeCard(drinkInfo) {
-//     const title = document.querySelector(".title")
-//     const image = document.querySelector(".thumb_nail")
-//     const ingredient1 = document.querySelector('.amount_ingredient1')
-//     const ingredient2 = document.querySelector('.amount_ingredient2')
-//     const ingredient3 = document.querySelector('.amount_ingredient3')
-//     const ingredient4 = document.querySelector('.amount_ingredient4')
-//     const ingredient5 = document.querySelector('.amount_ingredient5')
-//     const ingredient6 = document.querySelector('.amount_ingredient6')
-//     const ingredient7 = document.querySelector('.amount_ingredient7')
-//     const ingredient8 = document.querySelector('.amount_ingredient8')
-//     const instructions = document.querySelector('#instructions')
-//     title.innerText = drinkInfo.strDrink
-//     image.src = drinkInfo.strDrinkThumb
-//     ingredient1.innerText = `${drinkInfo.strMeasure1}: ${drinkInfo.strIngredient1}`
-//     ingredient2.innerText = `${drinkInfo.strMeasure2}: ${drinkInfo.strIngredient2}`
-//     ingredient3.innerText = `${drinkInfo.strMeasure3}: ${drinkInfo.strIngredient3}`
-//     ingredient4.innerText = `${drinkInfo.strMeasure4}: ${drinkInfo.strIngredient4}`
-//     ingredient5.innerText = `${drinkInfo.strMeasure5}: ${drinkInfo.strIngredient5}`
-//     ingredient6.innerText = `${drinkInfo.strMeasure6}: ${drinkInfo.strIngredient6}`
-//     ingredient7.innerText = `${drinkInfo.strMeasure7}: ${drinkInfo.strIngredient7}`
-//     ingredient8.innerText = `${drinkInfo.strMeasure8}: ${drinkInfo.strIngredient8}`
-//     instructions.innerText = drinkInfo.strInstructions
-// }
-//getDrinksByName('mojito')
